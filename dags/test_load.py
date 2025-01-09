3c8feacf95165bc363d39396578c00f1e00ceb45
Van Hong <vanhong542002@gmail.com>
2025-01-09 22:00:20 +0700

    update

--- dags/test_load.py
+++ dags/test_load.py
@@ -15,14 +15,32 @@ default_args = {
         "url": " ",
         "azure_storage_key": azure_storage_key,
         "dirs": [
-            "data/bronze/football_stadiums",
-            "data/bronze/country",
-            "data/bronze/continent"
+            "data/bronze/football_stadiums/",
+            "data/bronze/country/",
+            "data/bronze/continent/"
         ],
         "file_names": [
-            'football_stadiums.csv',
-            'country.csv',
-            'continent.csv'
+            'football_stadiums_',
+            'country_',
+            'continent_'
+        ],
+        "keys": [
+            "football_stadiums_data",
+            "country_data",
+            "continent_data"
         ]
     }
 }
+
+with DAG(
+    dag_id='test_load',
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+    load_raw_data = PythonOperator(
+        task_id="load_raw_data",
+        python_callable=load_raw_data,
+        provide_context=True
+    )
+    load_raw_data
--- src/pipelines/load/load_data.py
+++ src/pipelines/load/load_data.py
@@ -3,7 +3,7 @@ sys.path.insert(0, '/opt/airflow/src/')
 from objects.etl import ETL
 import json
 import pandas as pd
-
+from datetime import datetime
 
 def load_raw_data(**kwargs):
     """
@@ -14,18 +14,22 @@ def load_raw_data(**kwargs):
     url = kwargs['url']
     dirs = kwargs['dirs']
     file_names = kwargs['file_names']
-
     etl = ETL(url)
+    current_time = datetime.now().strftime("%Y_%m_%d_%H%M%S")
 
     for i in range(len(keys)):
         key = keys[i]
         dir = dirs[i]
-        file_name = file_names[i]
 
-        data = kwargs['ti'].xcom_pull(key=key, task_ids='extract_wikipedia_data')
-        data = pd.DataFrame(json.loads(data))
+        file_name = f"{file_names[i]}_{current_time}.csv"
+
+        data = kwargs['ti'].xcom_pull(key=key, 
+                                      task_ids='extract_wikipedia_data',
+                                      dag_id='test_extract',
+                                      include_prior_dates=True)
+        df = pd.DataFrame(json.loads(data))
         
-        etl.load(data, 
+        etl.load(df,
                  file_name=file_name, 
                  azure_storage_key=kwargs['azure_storage_key'], 
                  dir=dir)
