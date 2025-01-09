f5de9cdc0f7a72861961c6eb6e41475b1bb7893d
Van Hong <vanhong542002@gmail.com>
2025-01-09 21:31:15 +0700

    update

--- src/pipelines/extract/extract_raw_wikipedia_data.py
+++ src/pipelines/extract/extract_raw_wikipedia_data.py
@@ -1,10 +1,7 @@
 import sys
 sys.path.insert(0, '/opt/airflow/src/')
 from objects.etl import ETL
-import pandas as pd
-import json
-from datetime import datetime
-import re
+
 
 def extract_wikipedia_data(**kwargs):
     """
@@ -12,15 +9,23 @@ def extract_wikipedia_data(**kwargs):
     """
     # Setup variables
     urls = kwargs['urls']
-    target_table_index = kwargs['target_table_index']
-    for url in urls:
-        etl = ETL(url)
-
-        # Extract list of tables from the webpage
-        tables = etl.extract_()
-
-        # Get the target table and convert to json
-        json_target_table = tables[target_table_index].to_json(orient='records')
-        kwargs['ti'].xcom_push(key=f'{url}_data', value=json_target_table)
+    target_table_indexes = kwargs['target_table_indexes']
+    file_names = kwargs['file_names']
+    for i in range(len(urls)):
+        url = urls[i]
+        target_table_index = target_table_indexes[i]
+        file_name = file_names[i]
+    
+        # ETL process
+        etl = ETL(url)  # Tạo đối tượng ETL với URL hiện tại
+        tables = etl.extract_()  # Trích xuất tất cả các bảng từ URL
+    
+        # Kiểm tra xem index có hợp lệ không
+        if target_table_index < len(tables):
+            json_target_table = tables[target_table_index].to_json(orient='records')
+            kwargs['ti'].xcom_push(key=f'{file_name}_data', value=json_target_table)
+        else:
+            print(f"Error: URL '{url}' does not have table at index {target_table_index}")
     return "Data extracted and pushed to XCom"
+
 
