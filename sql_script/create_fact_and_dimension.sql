35129976462e6a310ab71c219b97e0a66eb4ac04
Van Hong <vanhong542002@gmail.com>
2025-01-12 13:38:22 +0700

    update

--- sql_script/create_fact_and_dimension.sql
+++ sql_script/create_fact_and_dimension.sql
@@ -13,7 +13,7 @@ CREATE TABLE dim_country (
     country_id VARCHAR(10) PRIMARY KEY,
     country_name VARCHAR(255),
     continent_id VARCHAR(10),
-    FOREIGN KEY (continent_id) REFERENCES Dim_Continent(continent_id)
+    FOREIGN KEY (continent_id) REFERENCES dim_continent(continent_id)
 );
 CREATE TABLE dim_stadiums (
     stadium_id INT PRIMARY KEY,
@@ -26,8 +26,9 @@ CREATE TABLE fact_footballstadiums (
     country_id VARCHAR(10),
     continent_id VARCHAR(10),
 	home_teams TEXT,
-    FOREIGN KEY (country_id) REFERENCES Dim_Country(country_id),
-    FOREIGN KEY (continent_id) REFERENCES Dim_Continent(continent_id)
+    FOREIGN KEY (country_id) REFERENCES dim_country(country_id),
+    FOREIGN KEY (continent_id) REFERENCES dim_continent(continent_id),
+    FOREIGN KEY (stadium_id) REFERENCES dim_stadiums(stadium_id)
 );
 
 -- CREATE A TEMP TABLE TO IMPORT DATA
@@ -35,10 +36,10 @@ CREATE TABLE temp_stadiums (
     stadium_id INT PRIMARY KEY,
     stadium VARCHAR(255),
     capacity INT,
+	city VARCHAR(255),
+	home_teams TEXT,
+	country VARCHAR(255),
+	country_id VARCHAR(255),
     continent VARCHAR(255),
-    country VARCHAR(255),
-    city VARCHAR(255),
-    home_teams TEXT,
-    country_id VARCHAR(255),
     continent_id VARCHAR(255)
 );
--- sql_script/insert_value_to_tables.sql
+++ sql_script/insert_value_to_tables.sql
@@ -1,3 +1,12 @@
+-- COPY DATA FROM ready_footballstadiums INTO temp_stadiums TO INSERT VALUE TO OTHER TABLES
+-- NOTE: CHANGE YOUR PATH TO DATA IN LOCAL
+INSERT INTO temp_stadiums (stadium_id, stadium, capacity, city, home_teams, country, country_id, continent, continent_id)
+SELECT stadium_id, stadium, capacity, city, home_teams, country, country_id, continent, continent_id
+FROM json_populate_recordset(
+  NULL::temp_stadiums, 
+  pg_read_file('E:/DataEngineerProject/Football-Stadiums-Analysis/data/GOLD/ready/ready_footballstadiums.json')::json
+);
+
 -- INSET VALUE FROM temp_stadiums TO OTHER TABLES
 INSERT INTO dim_continent (continent_id, continent_name)
 SELECT DISTINCT continent_id, continent
@@ -9,17 +18,15 @@ SELECT DISTINCT country_id, country, continent_id
 FROM temp_stadiums
 ON CONFLICT (country_id) DO NOTHING;
 
-INSERT INTO dim_stadiums (stadium_id, stadium_name, home_teams, capacity, city)
-SELECT DISTINCT stadium_id, stadium, home_teams, capacity, city
+INSERT INTO dim_stadiums (stadium_id, stadium_name, capacity, city)
+SELECT DISTINCT stadium_id, stadium, capacity, city
 FROM temp_stadiums
 ON CONFLICT (stadium_id) DO NOTHING;
 
-INSERT INTO fact_footballstadiums (stadium_id, country_id, continent_id)
-SELECT DISTINCT stadium_id, country_id, continent_id
+INSERT INTO fact_footballstadiums (stadium_id, country_id, continent_id, home_teams)
+SELECT DISTINCT stadium_id, country_id, continent_id, home_teams
 FROM temp_stadiums
 ON CONFLICT (stadium_id) DO NOTHING;
 
 -- DROP TEMP TABLE AFTER INSERT VALUE INTO FACT AND DIMENSIONS
 DROP TABLE IF EXISTS temp_stadiums;
-
-
